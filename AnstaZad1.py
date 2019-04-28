89a99559c440848121d12dee462dab4459574f7a
Aleksandra Zych <45856462+AleksandraZych@users.noreply.github.com>
2019-04-28 15:29:09 +0200

    Update AnstaZad1.py

--- AnstaZad1.py
+++ AnstaZad1.py
@@ -1,24 +1,11 @@
+import re
+
 c="79-900"
 b="80-155"
-def change(a):
-    a.split()
-    s=[]
-    for i in a:
-        try: 
-            int(i)
-            s.append(int(i))
-        except:
-            continue
-    iterator=1
-    number=0
-    for i in reversed( range(len(s))):
-        number+=s[i]*iterator
-        iterator=iterator*10
-    return number
-    
+
 def code(start,stop):
-    start=change(c)
-    stop=change(b)
+    start=re.sub('-','',c)
+    stop=re.sub('-','',b)
     result=[]
     for i in range(start, stop, 1):
         result.append(i)
